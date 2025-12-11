9af64e1e54e0b4be73603e63533d3c917c49ff03
nid-22 <archiquake@gmail.com>
2025-12-11 12:58:44 +0530

    Stop tracking __pycache__/.pyc files; update .gitignore

--- vasta_portfolio/vasta_portfolio/settings.py
+++ vasta_portfolio/vasta_portfolio/settings.py
@@ -136,10 +136,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATICFILES_DIRS = [
-#     BASE_DIR / "static",
-# ]
-STATIC_ROOT = BASE_DIR / 'static' #for deployment
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+# STATIC_ROOT = BASE_DIR / 'static' #for deployment
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
